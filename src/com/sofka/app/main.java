2963f20ea2fb71240da6c2d8c717a86dd6dd9900
Cgalvispadilla <fktcg99@gmail.com>
2021-09-01 11:22:30 -0500

    se agrego la funcionalidad de suma en el proyecto

--- src/com/sofka/app/main.java
+++ src/com/sofka/app/main.java
@@ -1,4 +1,14 @@
 package com.sofka.app;
 
 public class main {
+
+    public static void main(String[] args) {
+
+    }
+}
+class Operaciones{
+    public static int suma(int numero1, int numero2){
+        return numero1+numero2;
+    }
+
 }
